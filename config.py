0ad9a8b5b3fd5f399a1ed77da6ae5c5d0991a9be
JerryMazeyu <jerry_mazeyu@163.com>
2021-07-07 09:10:30 +0800

    add .gitignore

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Option:
         self.maze_blind_num = 2  # 生成迷宫的死路最小数量
         self.perception_range = 4  # 感知的范围半径
         self.init_score = 100  # 初始分数
-        self.maze_file_path = "/Users/mazeyu/PythonProject/2021.05/SuperChaser/maze.txt"
+        self.maze_file_path = "/Users/mazeyu/PythonProject/2021.05/SuperChaser/maze.txt"  #
 
 
 option = Option()
